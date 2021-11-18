b65fbd554ca56358597d25d4ff897ee95c58b50f
jorgencode <hello@jorgencode.com>
2021-11-18 12:34:52 +0100

    dinner vote app finished

--- food/src/App.js
+++ food/src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import DinnerVote from './components/dinnerVote';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <DinnerVote img="https://static.pexels.com/photos/46239/salmon-dish-food-meal-46239.jpeg"/>
+        <DinnerVote img="https://static.pexels.com/photos/58722/pexels-photo-58722.jpeg"/>
+        <DinnerVote img="https://static.pexels.com/photos/70497/pexels-photo-70497.jpeg"/>
+      </div>
   );
 }
 
---
+++ food/src/components/dinnerVote.js
@@ -0,0 +1,43 @@
+import React, {useState} from 'react'
+
+function DinnerVote(props) {
+
+    const [ useStateVote, setStateFood] = useState(0)
+
+    function add(){
+      setStateFood(useStateVote + 1)
+    }
+    function subtract(){
+      setStateFood(useStateVote - 1) 
+    }
+    var dinnerVoteStyle = {
+        // From Friendly
+        display: "inline-block",
+        textAlign: "center",
+        margin: 20,
+        height: 500,
+        width: 300,
+        padding: 0,
+        backgroundColor: "#FFF",
+        WebkitFilter: "drop-shadow(0px 0px 5px #555)",
+        filter: "drop-shadow(0px 0px 5px #555)"
+      }
+      var foodStyle = {
+        height: 200,
+        width: 200,
+        marginTop: 20
+      }
+    return (
+        <div style={dinnerVoteStyle}>
+            <img src={props.img} alt="food image" style={foodStyle}/>
+            <h1>Dinner Time</h1>
+            <p>Vote Here If You Want This</p>
+            <button onClick={add}>+1</button>
+            <p>Vote Here If You Do Not Want This</p>
+            <button onClick={subtract}>-1</button>
+            <p>Total: {useStateVote}</p>
+          </div>
+    )
+}
+
+export default DinnerVote
